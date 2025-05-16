f79229364a07ab01f538121455d57dc457bc03ce
Sameer Shaik <sameer.kattubadi@gmail.com>
2025-05-16 09:47:18 +0530

    api route handle airdrop

---
+++ src/app/api/airdrop/route.ts
@@ -0,0 +1,28 @@
+import { CLUSTER_API_URL } from "@/utils/helper";
+import { LAMPORTS_PER_SOL, Connection, PublicKey } from "@solana/web3.js";
+import { NextRequest, NextResponse } from "next/server";
+
+const connection = new Connection(CLUSTER_API_URL.DEVNET, "confirmed");
+
+export async function POST(req: NextRequest) {
+  try {
+    const data = await req.json();
+    const { publicKey } = await data;
+
+    const ValidPublicKey = new PublicKey(publicKey);
+    const AirDropSignature = await connection.requestAirdrop(
+      ValidPublicKey,
+      1 * LAMPORTS_PER_SOL
+    );
+    return NextResponse.json(
+      { message: `Airdropped to ${publicKey}`, signature: AirDropSignature },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "Error while airdropping" },
+      { status: 400 }
+    );
+  }
+}
